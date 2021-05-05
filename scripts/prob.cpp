020634f7e01dfc9f2784519403945c22c2a7dcdf
seiichiinoue <d35inou108@gmail.com>
2021-05-05 17:39:24 +0000

    added calculation of marginal pw

--- scripts/prob.cpp
+++ scripts/prob.cpp
@@ -7,7 +7,7 @@ bool compare_by_b(std::pair<wstring, double> a, std::pair<wstring, double> b) {
         return a.first < b.first;
     }
 }
-std::vector<std::pair<wstring, double>> word_ranking(SCANTrainer &trainer, int t, int k, int top_n=10, bool prob=false) {
+std::vector<std::pair<wstring, double>> word_ranking(SCANTrainer &trainer, int t, int k) {
     std::vector<std::pair<wstring, double>> pw;
     for (int v=0; v<trainer._scan->_vocab_size; ++v) {
         if (trainer._word_frequency[v] < trainer._ignore_word_count) {
@@ -25,6 +25,27 @@ std::vector<double> sense_probability(SCANTrainer &trainer, int t) {
         ps.push_back(trainer._logistic_Phi[t][k]);
     }
     return ps;
+}
+std::vector<std::pair<wstring, double>> marginal_word_ranking(SCANTrainer &trainer, int k) {
+    std::unordered_map<int, double> id_prob;
+    std::vector<std::pair<wstring, double>> pw;
+    for (int t=0; t<trainer._scan->_n_t; ++t) {
+        for (int v=0; v<trainer._scan->_vocab_size; ++v) {
+            if (trainer._word_frequency[v] < trainer._ignore_word_count) {
+                continue;
+            }
+            id_prob[v] += trainer._logistic_Psi[t][k][v];
+        }
+    }
+    for (int v=0; v<trainer._scan->_vocab_size; ++v) {
+        if (trainer._word_frequency[v] < trainer._ignore_word_count) {
+            continue;
+        }
+        wstring word = trainer._vocab->word_id_to_string(v);
+        pw.push_back(make_pair(word, id_prob[v]));
+    }
+    sort(pw.begin(), pw.end(), compare_by_b);
+    return pw;
 }
 
 DEFINE_string(model_path, "./bin/transport.model", "path to model archive");
@@ -51,6 +72,14 @@ int main(int argc, char *argv[]) {
             }
             wcout << endl;
         }
+    }
+    wcout << "p(w|k) = sum_t p(w|t,k):" << endl;
+    for (int k=0; k<trainer._scan->_n_k; ++k) {
+        std::vector<std::pair<wstring, double>> mpw = marginal_word_ranking(trainer, k);
+        for (int i=0; i<10; ++i) {
+            wcout << mpw[i].first << " ";
+        }
+        wcout << endl;
     }
     return 0;
 }
