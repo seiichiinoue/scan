dc1dea7cfea2b6eb1728532e3f6c181a934bf343
seiichiinoue <d35inou108@gmail.com>
2022-01-10 03:07:07 +0000

    update test scripts

--- tests/sample_data.py
+++ tests/sample_data.py
@@ -1,10 +1,13 @@
 from os import path
-import numpy as np
 import argparse
+import numpy as np
+import matplotlib.pyplot as plt
 
 PROJECT_TOP = "/workspace"
 OUTPUT_PREFIX = path.join(PROJECT_TOP, "tests/sampled")
 OUTPUT_PATH = path.join(OUTPUT_PREFIX, "pseudo_data.txt")
+
+np.random.seed(0)
 
 class Sense:
     def __init__(self):
@@ -18,7 +21,9 @@ class Sense:
         else:
             self.priors = [0 for _ in range(t - change_point + 1)] + self.priors[:-1]
 
-    def set_smoothed_probs(self, t: int = 16, change_point: int = 8, reverse: bool = False):
+    def set_smoothed_probs(self, t: int = 16,
+                           change_point: int = 8,
+                           reverse: bool = False):
         def f(x):
             return 1 / (1 + np.exp(-x))
         
@@ -29,7 +34,9 @@ class Sense:
         for i in range(t):
             self.priors.append(f(slices[i]))
 
-    def set_scurve_probs(self, t: int = 16, change_point: int = 8, reverse: bool = False):
+    def set_scurve_probs(self, t: int = 16,
+                         change_point: int = 8,
+                         reverse: bool = False):
         assert t > change_point
         slices = np.arange(-3, 3, 6/t)
         for i in range(t):
@@ -38,6 +45,22 @@ class Sense:
             else:
                 self.priors.append(slices[i])
 
+    def set_random_curve_probs_with_gaussian_process(self, t: int = 16):
+        def mean_function(x):
+            return np.zeros_like(x)
+        
+        def covariance_function(x1, x2, s1, s2):
+            return np.exp(-((x1 - x2) ** 2) / (s2 ** 2))
+
+        x = np.linspace(0, t-1, t)
+        x1, x2 = np.meshgrid(x, x)
+        s1, s2 = 2.0, 5.0
+        m = mean_function(x)
+        gram_matrix = covariance_function(x1, x2, s1, s2)
+        sample = np.random.multivariate_normal(m, gram_matrix)
+        self.priors = sample.tolist()
+        
+
 class Sampler:
     def __init__(self,
                  num_times: int = 16,
@@ -45,7 +68,8 @@ class Sampler:
                  context_window_size: int = 10,
                  vocab_size_per_sense: int = 3,
                  ratio_common_vocab: float = 0.2,
-                 shift_type: str = "default"):
+                 shift_type: str = "default",
+                 word_prior_type: str = "dirichlet"):
         self.num_times = num_times
         self.num_senses = num_senses
         self.context_window_size = context_window_size
@@ -59,21 +83,30 @@ class Sampler:
                 sense.set_scurve_probs(self.num_times, reverse=bool(i))
             elif shift_type == "smoothed":
                 sense.set_smoothed_probs(self.num_times, reverse=bool(i))
+            elif shift_type == "random":
+                sense.set_random_curve_probs_with_gaussian_process(self.num_times)
             self.senses.append(sense)
         xs = np.array([s.priors for s in self.senses]).T
         self.probs = np.array([self.softmax(x) for x in xs])
         self.vocab_size_per_sense = vocab_size_per_sense
         self.ratio_common_vocab = ratio_common_vocab
-        # hypothesize uniform distribution
+        # hypothesize power-low distribution
         # vocab size = num_sense * vocab_size_per_sense
         # e.g. num_sense = 2, vocab_size_per_sense = 3 then vocab size is 6
         self.word_probs = []
         for sense in range(self.num_senses):
-            start_idx = sense * self.vocab_size_per_sense
-            end_idx = (sense + 1) * self.vocab_size_per_sense
-            probs = [0 if i < start_idx or i >= end_idx 
-                     else (1.0 / self.vocab_size_per_sense)
-                     for i in range(self.vocab_size_per_sense * self.num_senses)]
+            cnt_pre = sense * self.vocab_size_per_sense
+            cnt_post = (self.num_senses - sense - 1) * self.vocab_size_per_sense
+            if word_prior_type == "dirichlet":
+                probs = [0.0 for _ in range(cnt_pre)] \
+                    + np.random.dirichlet(
+                        [1 for _ in range(self.vocab_size_per_sense)]
+                    ).tolist() \
+                    + [0.0 for _ in range(cnt_post)]
+            elif word_prior_type == "uniform":
+                probs = [0.0 for _ in range(cnt_pre)] \
+                    + [1.0 / self.vocab_size_per_sense for _ in range(self.vocab_size_per_sense)] \
+                    + [0.0 for _ in range(cnt_post)]
             self.word_probs.append(probs)
         self.id_to_token = []
         num_common_vocab = int(self.vocab_size_per_sense * self.ratio_common_vocab)
@@ -110,6 +143,28 @@ class Sampler:
                     f.write("{} {}\n".format(str(t), " ".join(snippet)))
 
 
+def plot_curve(t, senses):
+    x = np.linspace(0, t-1, t)
+    for sense in senses:
+        plt.plot(x, sense.priors)
+    plt.savefig(f'tests/fig/gp_sense{str(len(senses))}.png')
+
+def plot_proportion(probs):
+    plt.rc('legend', fontsize=13)
+    # probs = np.array(probs)[:, [2, 1, 0]]
+    # probs = np.array(probs)[:, [1, 2, 4, 3, 0]]
+    fig, ax = plt.subplots(figsize=(10, 5))
+    plt.xticks(fontsize=13)
+    plt.yticks(fontsize=13)
+    legend = []
+    for i in range(len(probs[0])):
+        ax.bar([str(j) for j in range(len(probs))], probs[:, i], bottom=probs[:, :i].sum(axis=1))
+        legend.append(f"sense_{str(i)}")
+    plt.legend(legend, loc='upper left', bbox_to_anchor=(0, -0.1), ncol=5)
+    fig.tight_layout()
+    plt.savefig(f'tests/fig/gp_prob_sense{str(len(probs[0]))}.png')
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--num-times', type=int, default=16)
@@ -118,8 +173,9 @@ if __name__ == "__main__":
     parser.add_argument('--vocab-size-per-sense', type=int, default=10)
     parser.add_argument('--ratio-common-vocab', type=float, default=0.2)
     parser.add_argument('--num-sample', type=int, default=100)
-    parser.add_argument('--shift-type', type=str, default="s-curve")
+    parser.add_argument('--shift-type', type=str, default="random")
     parser.add_argument('--output-path', type=str, default="pseudo_data.txt")
+    parser.add_argument('--word-prior-type', type=str, default="dirichlet")
     args = parser.parse_args()
 
     OUTPUT_PATH = args.output_path
@@ -130,6 +186,9 @@ if __name__ == "__main__":
         context_window_size=args.context_window_size,
         vocab_size_per_sense=args.vocab_size_per_sense,
         ratio_common_vocab=args.ratio_common_vocab,
-        shift_type=args.shift_type
+        shift_type=args.shift_type,
+        word_prior_type=args.word_prior_type
         )
+    plot_curve(args.num_times, sampler.senses)
+    plot_proportion(sampler.probs)
     sampler.draw_words(n_sample=args.num_sample)
--- tests/test.sh
+++ tests/test.sh
@@ -1,30 +1,33 @@
 # parameters
 # sampler
 NUM_TIMES=16
-NUM_SENSES=2
+NUM_SENSES=$1
 CONTEXT_WINDOW_SIZE=10
-VOCAB_SIZE_PER_SENSE=100
-RATIO_COMMON_VOCAB=0.5
+VOCAB_SIZE_PER_SENSE=1000
+RATIO_COMMON_VOCAB=0.0
 NUM_SAMPLE=1000
-SHIFT_TYPE="s-curve"
+SHIFT_TYPE="random"
+WORD_PRIOR_TYPE="dirichlet"
 
 # sb-scan
-NUM_ITERATION=1000
+NUM_ITERATION=2000
 
 DAY=$(date "+%m%d")
-INPUT_DATA=./tests/sampled/pseudo_sense${NUM_SENSES}_vocab${VOCAB_SIZE_PER_SENSE}_window${CONTEXT_WINDOW_SIZE}_sample${NUM_SAMPLE}.txt
-BINARY_PATH=./bin/pseudo_$DAY.model
-LOG_PATH=./log/out_pseudo_$DAY
+INPUT_DATA=./tests/sampled/pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_vocab${VOCAB_SIZE_PER_SENSE}_common${RATIO_COMMON_VOCAB}_window${CONTEXT_WINDOW_SIZE}_sample${NUM_SAMPLE}.txt
+SUFFIX=pseudo_${WORD_PRIOR_TYPE}_sense${NUM_SENSES}_sample${NUM_SAMPLE}_vocab${VOCAB_SIZE_PER_SENSE}_kappa_psi${KAPPA_PSI}_top_n${TOP_N_WORD}_${DAY}
+BINARY_PATH=./bin/${SUFFIX}.model
+LOG_PATH=./log/out_${SUFFIX}
 
 # generate pseudo data
 python3 tests/sample_data.py --num-times $NUM_TIMES \
-                           --num-senses $NUM_SENSES \
-                           --context-window-size $CONTEXT_WINDOW_SIZE \
-                           --vocab-size-per-sense $VOCAB_SIZE_PER_SENSE \
-                           --ratio-common-vocab $RATIO_COMMON_VOCAB \
-                           --num-sample $NUM_SAMPLE \
-                           --shift-type $SHIFT_TYPE \
-                           --output-path $INPUT_DATA
+                             --num-senses $NUM_SENSES \
+                             --context-window-size $CONTEXT_WINDOW_SIZE \
+                             --vocab-size-per-sense $VOCAB_SIZE_PER_SENSE \
+                             --ratio-common-vocab $RATIO_COMMON_VOCAB \
+                             --num-sample $NUM_SAMPLE \
+                             --shift-type $SHIFT_TYPE \
+                             --word-prior-type $WORD_PRIOR_TYPE \
+                             --output-path $INPUT_DATA
 # execute test
 ./scan -data_path=$INPUT_DATA \
        -save_path=$BINARY_PATH \
@@ -36,4 +39,4 @@ python3 tests/sample_data.py --num-times $NUM_TIMES \
        -num_iteration=$NUM_ITERATION > $LOG_PATH
 
 # output probabilities
-./prob -model_path=$BINARY_PATH -use_npmi=false > scripts/out/pseudo_$DAY
+./prob -model_path=$BINARY_PATH -use_npmi=false > scripts/out/${SUFFIX}
